6189aa9a2d1f663f2410f07461a9a6a0330028c4
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-04-21 11:01:10 -0600

    removed junk from testing

--- src/extractInterfaceListener.ts
+++ src/extractInterfaceListener.ts
@@ -31,39 +31,5 @@ export class ExtractInterfaceListener implements JavaListener {
         }
         let args = tokens.getText(ctx.formalParameters());
         console.log(`\t${type} ${ctx.Identifier()}${args};`);
-        // let params = ctx.formalParameters();
-        // let args = tokens.getText(params);
-        // this.methods.push(`${type} ${ctx.Identifier()}${args}`);
-        // this.methods.push(`${ctx.Identifier()} ${ctx.Identifier()}${ctx.methodDeclaratorRest}`);
-
-        // console.log("\t"+type+" "+ctx.Identifier()+args+";");
-        // let identifier: string;
-        // let vis: string;
-        // let returns: string;
-
-        // ctx.children.forEach(child  => {
-        //     if(child instanceof TerminalNode){
-        //         let tNode = <TerminalNode> child;
-        //         if(tNode.symbol.type == 120){
-        //             //console.log(tNode.symbol.text);
-        //             identifier = tNode.symbol.text;
-        //         }else if(tNode.symbol.type == 57){
-        //             console.log(tNode.symbol.text);
-        //             returns = tNode.symbol.text;
-        //         }else if(tNode.symbol.type == 44){
-        //             // console.log(tNode.symbol.text);
-        //             vis = tNode.symbol.text;
-        //         }else{
-        //             // console.log(child.text);
-        //         }
-        //     }
-
-        // });
-        // console.log(
-        // `/*
-        //   * ${identifier} ${vis}
-        //   * @return ${returns}
-        //   */
-        // `)
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 'use strict';
 
-import { ANTLRInputStream, CommonTokenStream,  } from 'antlr4ts';
+import {ANTLRInputStream, CommonTokenStream} from 'antlr4ts';
 import {TerminalNode, ParseTreeWalker}  from 'antlr4ts/tree';
-// import {HelloLexer} from './grammar/HelloLexer'
-// import {HelloParser} from './grammar/HelloParser'
 import {JavaLexer} from './grammar/JavaLexer'
 import {JavaParser} from './grammar/JavaParser'
 import {ExtractInterfaceListener} from './extractInterfaceListener'
@@ -13,7 +11,7 @@ import * as fs from 'fs';
 import * as os from 'os';
 class Startup {
     public static main(): number {
-        fs.readFile('./testing/class.cls',(err, data) =>{
+        fs.readFile('./testing/Demo.java',(err, data) =>{
             if(!err){
                 Startup.walk(data.toString())
             }else{
@@ -25,7 +23,7 @@ class Startup {
     }
 
     public static walk(data:string){
-    // Create the lexer and parser
+        // Create the lexer and parser
         let inputStream = new ANTLRInputStream(data);
         let lexer = new JavaLexer(inputStream);
         let tokenStream = new CommonTokenStream(lexer);
@@ -34,18 +32,8 @@ class Startup {
         var tree = parser.compilationUnit();
         let printer = new ExtractInterfaceListener(parser);
         ParseTreeWalker.DEFAULT.walk(printer, tree);
-
-        // // Parse the input, where `compilationUnit` is whatever entry point you defined
-        // let result = parser.methodDeclaration();
-
-        // result.children.forEach((item, index)  => {
-        //     let token = <TerminalNode> item;
-        //     // console.log(`@${index}${item.symbol.start}'${item.text}'`);
-        //     console.log(`@${index},${token.symbol.startIndex}:${token.symbol.stopIndex}'${item.text}'<${token.symbol.type}>,${token.symbol.line}:${token.symbol.charPositionInLine}`);
-        // });
     }
 }
-
 
 
 Startup.main();
